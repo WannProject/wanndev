6356aa50ff661f264f36aeb061b53c78db384175
wannproject <wannproject20@gmail.com>
2024-06-24 01:51:58 +0800

    update gambar

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const TopLeftImg = () => {
   return (
     <div className="absolute left-0 top-0 mix-blend-color-dodge z-100 w-[200px] xl:w-[400px] opacity-50">
-      <Image src="/Top-left-img.png" width={400} height={400} alt="" />
+      <Image src="/image-top.png" width={400} height={400} alt="" />
     </div>
   );
 };
